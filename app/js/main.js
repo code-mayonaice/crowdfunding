729ea8f52836e016d79e44470e9057b75766e930
code-mayonaice <revillanhoel@gmail.com>
2021-04-05 16:16:08 +0800

    khllkhjlh

--- app/js/main.js
+++ app/js/main.js
@@ -6,8 +6,10 @@ const hamburderlogo = document.querySelector('.hamburger-logo');
 const remover = document.querySelector('.remover');
 const opacity = document.querySelector('.opacity') 
 const hamburgernav = document.querySelector('.hamburger-nav') 
-const bodyall = document.querySelector('.body-content');
-const selections = document.querySelectorAll('.selections');
+const modalpupup = document.querySelector('.popup-modal');
+const btn = document.querySelectorAll('.reward');
+
+
 
 hamburderlogo.addEventListener('click',()=>{
    remover.style.display = "block";
@@ -32,15 +34,8 @@ remover.addEventListener('click',()=>{
  });
 
 
-selections.forEach(function(select){ //add listener in every selections
- 
-   select.addEventListener('click',()=>{
-      select.style.border = "2px solid blue";
-      opacity.style.display = "block";
-      bodyall.style.zIndex = "10";
-   });
 
-});
+ //modal popup
 
 
 
